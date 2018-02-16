679bc92bfd712999c7a3d07254d85daead05bd43
eric_ferrante <eric_ferrante@CHS-Ferrante.chs.win.fuhsd.org>
2018-02-16 08:44:10 -0800

    Updated Room and CommandTake so that items can only be taken from the current room using "take [item]" and items can only be taken from containers by using "take [item] from [container]".  Previously, the Room commands take(item), has(item), remove(item) would also search all open containers.

--- TextAdventureStarter/command/CommandTake.java
+++ TextAdventureStarter/command/CommandTake.java
@@ -1,5 +1,6 @@
 package command;
 
+import interfaces.Container;
 import items.Item;
 import textadventure.World;
 
@@ -12,7 +13,8 @@ public class CommandTake extends Command {
 
 	@Override
 	public void doCommand(String cmd, String[] params, World world) {
-		if (params.length >= 1) {
+		if (params.length == 1) {
+			System.out.println("Section 1");
 			String secondWord = params[0];
 			if (world.getPlayer().getCurrentRoom().hasItem(secondWord)) {
 				Item item = world.getPlayer().getCurrentRoom().getItem(secondWord);
@@ -23,13 +25,60 @@ public class CommandTake extends Command {
 			else
 				World.print("You can't see any " + secondWord + " here.\n\n");
 		}
-		else
-			World.print("What do you want to take?\n\n");
+		else if (params.length == 3) {
+			System.out.println("Section 2");
+			String item = params[0];
+			String preposition = params[1];
+			String container = params[2];
+			// Handle errors
+			if (!preposition.equals("from")) {
+				World.print("I'm not sure what you mean.\n\n");
+				return;
+			}
+			// If the player already has this item in their inventory
+			if (world.getPlayer().hasItem(item)) {
+				World.print("You're already holding the " + item + ".\n\n");
+				return;
+			}
+
+			Item theContainer = world.getPlayer().hasItem(container) ? world.getPlayer().getItem(container) : world.getPlayer().getCurrentRoom().getItem(container);
+
+			// If there is not such container in this room or player's inventory
+			if (theContainer == null) {
+				World.print("You can't see any " + container + " here.\n\n");
+				return;
+			}
+
+			// If the container isn't actually a Container object
+			if (! (theContainer instanceof Container)) {
+				World.print("The " + container + " can't hold things!\n\n");
+				return;
+			}
+			
+			Item theItem = ((Container)theContainer).getItem(item);
+
+			// If the container doesn't contain the item requested
+			if (theItem == null) {
+				World.print("You can't see a " + item + " in the " + container + ".\n\n");
+				return;
+			}
+
+			// If the requested item is NOT_TAKEABLE
+			if (!theItem.isTakeable()) {
+				World.print("The " + item + " is part of the " + container + " and can't be taken.\n\n");
+				return;
+			}
+
+			// If we made it this far, then it's safe to take the item from the container
+			((Container)theContainer).removeItem(item);
+			world.getPlayer().addItem(theItem);
+			World.print("Taken.\n\n");
+		}
 	}
 
 	@Override
 	public String getHelpDescription() {
-		return "[item]";
+		return "[item] or take [Item] from [Container]";
 	}
 
 }
