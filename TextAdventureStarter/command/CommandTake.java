15986f01de9957fdef23b1652cdcdb695b6a2aa0
eric_ferrante <eric_ferrante@CHS-Ferrante.chs.win.fuhsd.org>
2018-02-20 10:15:59 -0800

    Reset CommandTake so that it only knows about take [Item] and not take [Item] from [Container] (which is added during the tutorial)

--- TextAdventureStarter/command/CommandTake.java
+++ TextAdventureStarter/command/CommandTake.java
@@ -1,6 +1,5 @@
 package command;
 
-import interfaces.Container;
 import items.Item;
 import textadventure.World;
 
@@ -24,55 +23,8 @@ public class CommandTake extends Command {
 			else
 				World.print("You can't see any " + itemName + " here.\n\n");
 		}
-		else if (params.length == 3) {
-			String itemName = params[0];
-			String preposition = params[1];
-			String containerName = params[2];
-			// Handle errors
-			if (!preposition.equals("from")) {
-				World.print("I'm not sure what you mean.\n\n");
-				return;
-			}
-			// If the player already has this item in their inventory
-			if (world.getPlayer().hasItem(itemName)) {
-				World.print("You're already holding the " + itemName + ".\n\n");
-				return;
-			}
-
-			Item theContainer = world.getPlayer().hasItem(containerName) ? world.getPlayer().getItem(containerName) : world.getPlayer().getCurrentRoom().getItem(containerName);
-
-			// If there is not such container in this room or player's inventory
-			if (theContainer == null) {
-				World.print("You can't see any " + containerName + " here.\n\n");
-				return;
-			}
-
-			// If the container isn't actually a Container object
-			if (! (theContainer instanceof Container)) {
-				World.print("The " + containerName + " can't hold things!\n\n");
-				return;
-			}
-			
-			Item theItem = ((Container)theContainer).getItem(itemName);
-
-			// If the container doesn't contain the item requested
-			if (theItem == null) {
-				World.print("You can't see a " + itemName + " in the " + containerName + ".\n\n");
-				return;
-			}
-
-			// If the requested item is NOT_TAKEABLE
-			if (!theItem.isTakeable()) {
-				World.print("The " + itemName + " is part of the " + containerName + " and can't be taken.\n\n");
-				return;
-			}
-
-			// If we made it this far, then it's safe to take the item from the container
-			((Container)theContainer).doTake(theItem, (Container)theContainer);
-
-//			((Container)theContainer).removeItem(itemName);
-//			world.getPlayer().addItem(theItem);
-//			World.print("Taken.\n\n");
+		else {
+			World.print("I don't understand.\n\n");
 		}
 	}
 
