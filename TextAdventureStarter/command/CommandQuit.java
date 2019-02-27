253deb585f11d88cebdfab3ccbe027b64577a28f
Ted McLeod <ted_mcleod@fuhsd.org>
2019-02-26 18:55:07 -0800

    quit is now a command just like the other commands.

---
+++ TextAdventureStarter/command/CommandQuit.java
@@ -0,0 +1,26 @@
+package command;
+
+import textadventure.World;
+
+public class CommandQuit extends Command {
+
+	@Override
+	public String[] getCommandWords() {
+		return new String[] {"quit"};
+	}
+
+	@Override
+	public void doCommand(String cmd, String[] params, World world) {
+		World.print("\nAre you sure you want to quit (enter y to confirm)? ");
+		if (World.IN.nextLine().equalsIgnoreCase("y")) {
+			World.print("\nThank you for playing!  Good bye!\n\n");
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public String getHelpDescription() {
+		return "quits the game";
+	}
+
+}
